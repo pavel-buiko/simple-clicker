32ea0b442bd032752e8d1ceb2405204d801cfb5e
Павел Буйко <pashabuiko17102004@gmail.com>
2024-01-30 20:45:51 +0300

    Complited programm

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const div = document.querySelector("#numbers")
+
+
+const btn = document.querySelector(".button")
+
+
+let counter = 0;
+const Clicker = () => {
+    counter++
+    div.textContent = counter
+}
+
+
